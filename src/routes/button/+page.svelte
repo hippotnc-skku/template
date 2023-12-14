08e79e87f2ac4dfd921271f10e510712ff21c3c7
yoonheehan <grayman1234@naver.com>
2023-12-14 18:31:57 +0900

    모듈화 작업

--- src/routes/button/+page.svelte
+++ src/routes/button/+page.svelte
@@ -1,1 +1,28 @@
+<script lang="ts">
+	import HippoButton from "$lib/components/hippocomponent/button/HippoButton.svelte";
+</script>
 
+
+<HippoButton class="bg-hippNavy rounded-lg text-white px-3 py-2">
+  버튼 1
+</HippoButton>
+
+<HippoButton class="bg-hippNavy rounded-lg text-white px-3 py-2">
+  버튼 1
+</HippoButton>
+
+<HippoButton class="bg-hippNavy rounded-lg text-white px-3 py-2">
+  버튼 1
+</HippoButton>
+
+<HippoButton class="bg-hippNavy rounded-lg text-white px-3 py-2">
+  버튼 1
+</HippoButton>
+
+<HippoButton class="bg-hippNavy rounded-lg text-white px-3 py-2">
+  버튼 1
+</HippoButton>
+
+<HippoButton class="bg-hippNavy rounded-lg text-white px-3 py-2">
+  버튼 1
+</HippoButton>
