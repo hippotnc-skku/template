336310a684464aad8915075b07615ec62e18de5a
yoonheehan <grayman1234@naver.com>
2023-12-28 12:13:30 +0900

    pagination 다음 버튼 누를시 5개씩 이동 + pagination 오류 수정

--- src/lib/components/hippocomponent/pagination/HippoPagination.svelte
+++ src/lib/components/hippocomponent/pagination/HippoPagination.svelte
@@ -15,14 +15,14 @@
 	const previous = () => {
 		if (Number(activeUrl) - 5 < 1) return;
 		let query = new URLSearchParams($page.url.searchParams.toString());
-		query.set('page', (Number(activeUrl) - 5).toString());
+		query.set('page', (Math.floor((Number(activeUrl) - 5) / 5) * 5 + 1).toString());
 		goto(`${$page.url.pathname}?${query}`, { invalidateAll: true });
 	};
 	// 다음(한 페이지)으로 가기
 	const next = () => {
-		if (Number(activeUrl) + 5 > Math.ceil(totalCount / perPage)) return;
+		if (Math.ceil(Number(activeUrl) / 5) >= Math.ceil(Math.ceil(totalCount / perPage) / 5)) return;
 		let query = new URLSearchParams($page.url.searchParams.toString());
-		query.set('page', (Number(activeUrl) + 5).toString());
+		query.set('page', (Math.floor((Number(activeUrl) + 5) / 5) * 5 + 1).toString());
 		goto(`${$page.url.pathname}?${query}`, { invalidateAll: true });
 	};
 
