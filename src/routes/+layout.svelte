a89fb4a230c8d192788d9573bcc8cde81fce05fe
yoonheehan <grayman1234@naver.com>
2023-12-29 11:03:12 +0900

    template autho add

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,50 +1,5 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-	import SideBar from './SideBar.svelte';
-	import Header from './Header.svelte';
-	import Toast from '$lib/components/notification/Toast.svelte';
-	import Popup from '$lib/components/popup.svelte';
-	import Footer from './Footer.svelte';
+<script>
 	import '../app.css';
-	import Navbar from './Navbar.svelte';
-
-	let showSideBar: string;
-	// 로그인 한 사람의 이름을 가져옴
-
-	// 사이드바 열고닫기 함수(css이용하여)
-	function showSideBarFunction() {
-		if (showSideBar === 'transition duration-500 ease-in-out lg:translate-x-0 px-2 sm:px-0') {
-			showSideBar = 'transition duration-500 ease-in-out -translate-x-96 lg:translate-x-0';
-		} else {
-			showSideBar = 'transition duration-500 ease-in-out lg:translate-x-0 px-2 sm:px-0';
-		}
-	}
-
-	function setToFalse() {
-		showSideBar = 'transition duration-500 ease-in-out -translate-x-96 lg:translate-x-0';
-		document.getElementById('menu').classList.toggle('open');
-	}
 </script>
 
-<svelte:body
-	class="m-0 font-sans antialiased font-normal text-left leading-default text-base bg-gray-50 text-slate-500"
-/>
-<div class="flex min-h-screen">
-	<SideBar {showSideBar} on:close={setToFalse} />
-
-	<div
-		class="ease-soft-in-out min-h-screen rounded-xl lg:ml-60 transition-all duration-200 flex flex-col item-center w-full min-w-[560px] flex flex-col justify-between"
-	>
-		<Header on:sideBar={showSideBarFunction} />
-		<div class="md:px-2.5 lg:px-3.5 xl:px-10 pt-[2.5rem] mt-10 h-full">
-			<Navbar />
-			<slot />
-		</div>
-		<div class="mt-10">
-			<Footer />
-		</div>
-	</div>
-</div>
-
-<Toast />
-<!-- <Popup open={true} data={data.pops}><div slot="body">공지사항 기입</div></Popup> -->
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,109 @@
-<div>
-  <h1>Welcome to SvelteKit</h1>
-  <p class="text-3xl">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { enhance, applyAction } from '$app/forms';
+	import { goto } from '$app/navigation';
+
+	import Spinner from '$lib/components/helpers/spinners/Spinner.svelte';
+	import Logo from '$images/hippo_logo copy.png';
+	import {
+		HippoInput,
+		HippoCheckBox,
+		HippoNavyButton,
+		HippoTextButton
+	} from '$lib/components/hippocomponent';
+	import HippoWhiteFrame from '$lib/components/hippocomponent/HippoWhiteFrame.svelte';
+
+	export let form;
+	export let data;
+
+	let id = '';
+	let pw = '';
+	let load = false;
+	let checkboxCheck = false;
+
+	function remainIDPWIFFailedLog() {
+		load = true;
+		return async ({ result }) => {
+			console.log(result);
+			await applyAction(result);
+			if (result?.data?.status === 'success') {
+				goto('/button');
+			}
+			load = false;
+		};
+	}
+
+	if (data?.uid) {
+		id = data.uid;
+		checkboxCheck = true;
+	} else {
+		checkboxCheck = false;
+	}
+
+	function firstClick() {
+		console.log('firstClick');
+	}
+
+	function secondClick() {
+		console.log('firstClick');
+	}
+</script>
+
+<div class="flex justify-center items-center h-screen w-full background">
+	<HippoWhiteFrame class="px-10 pt-10 pb-14">
+		<div class="w-[350px]">
+			<div class="flex justify-center">
+				<img src={Logo} alt="logo" class="w-[200px] mb-5" />
+			</div>
+			<div class="text-[26px] font-bold mb-4">로그인</div>
+
+			<div class="text-[15px] mb-4">아이디와 비밀번호를 입력해주세요.</div>
+
+			<form method="POST" action="/?/login" use:enhance={remainIDPWIFFailedLog}>
+				<HippoInput class="mb-3 w-full" name="uid" placeholder="ID" bind:value={id} />
+
+				<HippoInput
+					class="mb-4 w-full"
+					type="password"
+					name="password"
+					placeholder="password"
+					bind:value={pw}
+				/>
+
+				<HippoCheckBox name="rememberID" bind:checked={checkboxCheck}>아이디 기억하기</HippoCheckBox
+				>
+
+				{#if form?.status && form?.status !== 'success'}
+					<div class="text-red-600">잘못된 아이디 또는 비밀번호 입니다.</div>
+				{/if}
+
+				<HippoNavyButton class="w-full h-[46px] mt-4" type="submit">
+					{#if load}
+						<Spinner />
+					{:else}
+						로그인
+					{/if}
+				</HippoNavyButton>
+			</form>
+
+			<div class="flex justify-between items-start mt-8">
+				<div class="text-[#afafaf] text-[13px]">아이디/비밀번호를 잊어버리셨나요?</div>
+
+				<div class="flex">
+					<HippoTextButton on:click={firstClick} class="text-[13px]">아이디</HippoTextButton>
+					<div class="mx-0.5 text-primary text-[13px]">/</div>
+					<HippoTextButton on:click={secondClick} class="text-[13px]">비밀번호 찾기</HippoTextButton
+					>
+				</div>
+			</div>
+		</div>
+	</HippoWhiteFrame>
 </div>
+
+<style>
+	.background {
+		background-image: url('src/lib/images/loginImg.png');
+		background-repeat: no-repeat;
+		background-position: center center;
+		background-size: contain;
+	}
+</style>
