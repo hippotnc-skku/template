c4513e616d06a197723ddf9004105d5e1d89e2c4
yoonheehan <grayman1234@naver.com>
2024-01-15 17:38:42 +0900

    추가중

--- src/routes/(template)/component_frame/+page.svelte
+++ src/routes/(template)/component_frame/+page.svelte
@@ -11,16 +11,49 @@
 	</div>
 </HippoWhiteFrame>
 
+<CodeExampleFrame name="Setup">
+	<iframe
+		title="setup"
+		frameborder="0"
+		scrolling="no"
+		style="width:100%; height:103px;"
+		allow="clipboard-write"
+		src="https://emgithub.com/iframe.html?target=https%3A%2F%2Fgithub.com%2Fhippotnc-skku%2Ftemplate%2Fblob%2Fcomponent%2Fsrc%2Froutes%2F%28template%29%2Fcomponent_frame%2F_temp.svelte%3Fts%3D4%23L1-L3&style=github-dark&type=code&showBorder=on&showLineNumbers=on&showFullPath=on&showCopy=on"
+	></iframe>
+</CodeExampleFrame>
+
 <CodeExampleFrame>
 	<HippoWhiteFrame class="text-center py-5 px-4 space-y-1">
 		<div class="text-lg font-bold">Default</div>
 	</HippoWhiteFrame>
 </CodeExampleFrame>
 
+<CodeExampleFrame name="">
+	<iframe
+		title="default"
+		frameborder="0"
+		scrolling="no"
+		style="width:100%; height:103px;"
+		allow="clipboard-write"
+		src="https://emgithub.com/iframe.html?target=https%3A%2F%2Fgithub.com%2Fhippotnc-skku%2Ftemplate%2Fblob%2Fcomponent%2Fsrc%2Froutes%2F%28template%29%2Fcomponent_frame%2F_temp.svelte%3Fts%3D4%23L5-L7&style=github-dark&type=code&showBorder=on&showLineNumbers=on&showFullPath=on&showCopy=on"
+	></iframe>
+</CodeExampleFrame>
+
 <CodeExampleFrame name="width 80%인 경우">
 	<HippoWhiteFrame class="text-center py-5 w-[80%] mx-auto space-y-1">
 		<div class="text-lg font-bold">width: 80%인 경우</div>
 	</HippoWhiteFrame>
+</CodeExampleFrame>
+
+<CodeExampleFrame name="">
+	<iframe
+		title="80"
+		frameborder="0"
+		scrolling="no"
+		style="width:100%; height:103px;"
+		allow="clipboard-write"
+		src="https://emgithub.com/iframe.html?target=https%3A%2F%2Fgithub.com%2Fhippotnc-skku%2Ftemplate%2Fblob%2Fcomponent%2Fsrc%2Froutes%2F%28template%29%2Fcomponent_frame%2F_temp.svelte%3Fts%3D4%23L9-L11&style=github-dark&type=code&showBorder=on&showLineNumbers=on&showFullPath=on&showCopy=on"
+	></iframe>
 </CodeExampleFrame>
 
 <CodeExampleFrame name="padding 40px인 경우">
@@ -32,4 +65,15 @@
 			consequuntur illo suscipit doloribus.
 		</div>
 	</HippoWhiteFrame>
+</CodeExampleFrame>
+
+<CodeExampleFrame name="">
+	<iframe
+		title="padding"
+		frameborder="0"
+		scrolling="no"
+		style="width:100%; height:208px;"
+		allow="clipboard-write"
+		src="https://emgithub.com/iframe.html?target=https%3A%2F%2Fgithub.com%2Fhippotnc-skku%2Ftemplate%2Fblob%2Fcomponent%2Fsrc%2Froutes%2F%28template%29%2Fcomponent_frame%2F_temp.svelte%3Fts%3D4%23L13-L20&style=github-dark&type=code&showBorder=on&showLineNumbers=on&showFullPath=on&showCopy=on"
+	></iframe>
 </CodeExampleFrame>
