f8120fccd1f01abd95e410cca05e291a7f60127f
yoonheehan <grayman1234@naver.com>
2024-01-15 14:32:58 +0900

    input temp 추가

---
+++ src/routes/(template)/input/_temp.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { HippoInput } from '$lib/components/hippocomponent';
+</script>
+
+<HippoInput name="totalCount" placeholder="길이가 긴" class="w-full" />
+
+<HippoInput name="totalCount" placeholder="길이가 짧은" class="w-[80%]" />
+
+<HippoInput name="totalCount" placeholder="높이가 50px" class="w-full h-[50px]" />
+
+<HippoInput name="totalCount" placeholder="높이가 80px" class="w-full h-20" />
+
+<HippoInput name="totalCount" placeholder="패딩조절" class="w-full h-full" padding="px-10 py-10" />
+
+<HippoInput name="totalCount" placeholder="패딩조절" class="w-full h-full" padding="px-20 py-8" />
