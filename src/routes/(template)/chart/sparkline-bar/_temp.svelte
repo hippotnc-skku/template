24e0e515108b69fb4ea4e3f41f62f3b64ba3b7c5
getsetgo1 <dnwls1313@gmail.com>
2024-01-24 19:42:02 +0900

    sparklinebar temp

---
+++ src/routes/(template)/chart/sparkline-bar/_temp.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import SparkLineBar from '$lib/components/charts/chart_sparkline_bar.svelte';
+	const example=[{name:'예시', data:[13,18,23,15,19,21,17]}]
+</script>
+
+<SparkLineBar tooltipSuffix={"예시"} titles={"예시"} datas={example}/>
