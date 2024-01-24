bd0998ff7e0e4c9db2155a2814c87097c842bc7a
getsetgo1 <dnwls1313@gmail.com>
2024-01-24 19:33:28 +0900

    sparkline area temp 파일

---
+++ src/routes/(template)/chart/sparkline-area/_temp.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import SparkLineArea from '$lib/components/charts/template/chart_sparkline_area.svelte';
+	const example=[{name:'예시', data:[13,18,23,15,19,21,17]}]
+</script>
+
+<SparkLineLine tooltipSuffix={"예시"} titles={"예시"} datas={example}/>
