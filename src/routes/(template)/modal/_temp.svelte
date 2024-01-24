b991b041bdb12ceb3ed034992491851262ec10d5
getsetgo1 <dnwls1313@gmail.com>
2024-01-24 20:30:50 +0900

    모달 temp

---
+++ src/routes/(template)/modal/_temp.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import HippoWhiteFrame from '$lib/components/hippocomponent/HippoWhiteFrame.svelte';
+	import { Modal } from '$lib/components/helpers';
+
+	let series = [
+		{
+			name: 'name',
+			data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+		}
+	];
+	let chartHeight = 350;
+	let dataLabels = false;
+	let strokeCurve = 'straight';
+	let strokeWidth = 5;
+	let titleText = 'title';
+	let titleAlign = 'left';
+	let titleFontSize = '20px';
+	let titleFontWeight = 'bold';
+	let grid = true;
+	let gridColor = '#f3f3f3';
+	let gridBorderColor = '#90A4AE';
+	let gridOpacity = 0.5;
+	let xaxis = true;
+	let xaxisCategories = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'];
+	let xaxisTitle = 'xaxis title';
+	let xaxisLablesRotate = -45;
+	let yaxis = true;
+	let yaxisTitle = 'yaxis title';
+	let yaxisMin = 0;
+	let yaxisMax = 200;
+	let dropShadow = true;
+	let toolbar = true;
+	let markerSize = 1;
+	let markerStrokeColors = '#FFF';
+	let markerStrokeWidth = 2;
+	let markerStrokeOpacity = 0.9;
+	let legend = true;
+	let legendPosition = 'top';
+	let legendHorizontalAlign = 'center';
+	let lineColors = ['#77B6EA'];
+</script>
+<HippoWhiteFrame>
+<Modal title="모달창 제목" bind:open={editFamilyModal}>
+	<div class="text-black">모달창을 띄운 이유 (예시 :패밀리를 수정하시겠습니까?)</div>
+
+	<svelte:fragment slot="footer">
+		<form method="POST" action="/family/{$page.params.id}/edit/?/put" use:enhance={checkEditFamily}>
+			{#each Object.entries(submitFamily) as [key, value]}
+				<input type="hidden" name={key} bind:value={family[key]} />
+			{/each}
+			<HippoButton color="green" type="submit">수정</HippoButton>
+		</form>
+		<HippoButton color="gray" on:click={() => (editFamilyModal = false)}>닫기</HippoButton>
+	</svelte:fragment>
+</Modal>
+</HippoWhiteFrame>
---
+++ src/routes/(template)/modal/_temp2.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import HippoWhiteFrame from '$lib/components/hippocomponent/HippoWhiteFrame.svelte';
+	import { Modal } from '$lib/components/helpers';
+	import { notifications } from '$lib/components/notification/notifications';
+	let confirmBillModal=false;
+
+	function confirmBillState(){
+		notifications.success('Modal창이 떴습니다.')
+	}
+</script>
+<HippoWhiteFrame class="p-2 my-2">
+	<Modal title="청구 확정" bind:open={confirmBillModal}>
+		<div class="w-full max-w-full px-3 flex-0 mt-2 text-black">청구 확정하시겠습니까?</div>
+		<Button on:click={confirmBillState}>청구 확정</Button>
+		<Button color="alternative" on:click={() => (confirmBillModal = false)}>닫기</Button>
+	</Modal>
+</HippoWhiteFrame>
