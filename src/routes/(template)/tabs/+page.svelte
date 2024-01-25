89821981d970aba73ba27545b4bab6c061b77657
yoonheehan <grayman1234@naver.com>
2024-01-25 09:47:04 +0900

    체크박스 temp

--- src/routes/(template)/tabs/+page.svelte
+++ src/routes/(template)/tabs/+page.svelte
@@ -17,10 +17,32 @@
 	<div class="text-lg font-bold">Component는 따로 import하셔야 합니다.</div>
 </HippoWhiteFrame>
 
+<CodeExampleFrame name="Setup">
+	<iframe
+		title="setup"
+		frameborder="0"
+		scrolling="no"
+		style="width:100%; height:292px;"
+		allow="clipboard-write"
+		src="https://emgithub.com/iframe.html?target=https%3A%2F%2Fgithub.com%2Fhippotnc-skku%2Ftemplate%2Fblob%2Fcomponent%2Fsrc%2Froutes%2F%28template%29%2Ftabs%2F_temp.svelte%3Fts%3D4%23L1-L12&style=github-dark&type=code&showBorder=on&showLineNumbers=on&showFullPath=on&showCopy=on"
+	></iframe>
+</CodeExampleFrame>
+
 <CodeExampleFrame>
 	<HippoWhiteFrame class="px-8 py-5">
 		<HippoTabs {items} {tab} on:tabChange={(e) => (selected = options[e.detail.tab])}>
 			<svelte:component this={selected.component} />
 		</HippoTabs>
 	</HippoWhiteFrame>
+</CodeExampleFrame>
+
+<CodeExampleFrame>
+	<iframe
+		title="default"
+		frameborder="0"
+		scrolling="no"
+		style="width:100%; height:103px;"
+		allow="clipboard-write"
+		src="https://emgithub.com/iframe.html?target=https%3A%2F%2Fgithub.com%2Fhippotnc-skku%2Ftemplate%2Fblob%2Fcomponent%2Fsrc%2Froutes%2F%28template%29%2Ftabs%2F_temp.svelte%3Fts%3D4%23L14-L16&style=github-dark&type=code&showBorder=on&showLineNumbers=on&showFullPath=on&showCopy=on"
+	></iframe>
 </CodeExampleFrame>
