3daf7fb5f694b115266c11110ab58d946b329742
getsetgo1 <dnwls1313@gmail.com>
2024-01-29 11:35:36 +0900

    모달 수정

--- src/routes/(template)/modal/+page.svelte
+++ src/routes/(template)/modal/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import {HippoWhiteFrame,HippoButton} from '$lib/components/hippocomponent';
+	import { HippoWhiteFrame, HippoButton } from '$lib/components/hippocomponent';
 	import CodeExampleFrame from '$lib/codeboxframe/CodeExampleFrame.svelte';
 	import { List, Li, Heading } from 'flowbite-svelte';
 	import { Modal } from '$lib/components/helpers';
 	import { notifications } from '$lib/components/notification/notifications';
 
 	let editFamilyModal = false;
-	let confirmBillModal=false;
+	let confirmBillModal = false;
 	let submitFamily = {
 		name: '이름',
 		registration_name: '상호명',
@@ -31,7 +31,7 @@
 		hp: '포인트',
 		hfc: '크레딧'
 	};
-	let family={
+	let family = {
 		name: '',
 		registration_name: '',
 		phonenum: '',
@@ -54,7 +54,7 @@
 		area_code: '',
 		hp: '',
 		hfc: ''
-	}
+	};
 
 	function checkEditFamily() {
 		return async ({ result }) => {
@@ -72,23 +72,30 @@
 			}
 		};
 	}
-	function confirmBillState(){
-		notifications.success('Modal창이 떴습니다.')
+	function confirmBillState() {
+		notifications.success('함수 실행 성공', 1500);
+		confirmBillModal = false;
 	}
 </script>
 
 <CodeExampleFrame name="Sample1">
 	<HippoWhiteFrame class="p-2 my-2">
-		<HippoButton on:click={()=> (editFamilyModal = true)}>모달창 띄우기</HippoButton>
+		<HippoButton on:click={() => (editFamilyModal = true)}>모달창 띄우기</HippoButton>
 		<Modal title="모달창 제목" bind:open={editFamilyModal}>
 			<div class="text-black">모달창을 띄운 이유 (예시 :패밀리를 수정하시겠습니까?)</div>
-		
+
 			<svelte:fragment slot="footer">
-				<form method="POST" action="" use:enhance={checkEditFamily}>
+				<form method="POST" action="">
 					{#each Object.entries(submitFamily) as [key, value]}
 						<input type="hidden" name={key} bind:value={family[key]} />
 					{/each}
-					<HippoButton color="green" type="submit">수정</HippoButton>
+					<HippoButton
+						color="green"
+						on:click={() => {
+							notifications.success('폼 제출 성공', 1500);
+							editFamilyModal = false;
+						}}>수정</HippoButton
+					>
 				</form>
 				<HippoButton color="gray" on:click={() => (editFamilyModal = false)}>닫기</HippoButton>
 			</svelte:fragment>
@@ -97,11 +104,17 @@
 </CodeExampleFrame>
 
 <CodeExampleFrame name="Code1">
-	<iframe frameborder="0" scrolling="no" style="width:100%; height:1216px;" allow="clipboard-write" src="https://emgithub.com/iframe.html?target=https%3A%2F%2Fgithub.com%2Fhippotnc-skku%2Ftemplate%2Fblob%2Fcomponent%2Fsrc%2Froutes%2F%28template%29%2Fmodal%2F_temp.svelte%3Fts%3D4%23L1-L56&style=a11y-dark&type=code&showBorder=on&showLineNumbers=on&showFullPath=on&showCopy=on"></iframe>
+	<iframe
+		frameborder="0"
+		scrolling="no"
+		style="width:100%; height:1342px;"
+		allow="clipboard-write"
+		src="https://emgithub.com/iframe.html?target=https%3A%2F%2Fgithub.com%2Fhippotnc-skku%2Ftemplate%2Fblob%2Fcomponent%2Fsrc%2Froutes%2F%28template%29%2Fmodal%2F_temp.svelte%3Fts%3D4%23L1-L62&style=a11y-dark&type=code&showBorder=on&showLineNumbers=on&showFullPath=on&showCopy=on"
+	></iframe>
 </CodeExampleFrame>
 <CodeExampleFrame name="Sample2">
 	<HippoWhiteFrame class="p-2 my-2">
-	<HippoButton on:click={()=> (confirmBillModal = true)}>모달창 띄우기</HippoButton>
+		<HippoButton on:click={() => (confirmBillModal = true)}>모달창 띄우기</HippoButton>
 
 		<Modal title="청구 확정" bind:open={confirmBillModal}>
 			<div class="w-full max-w-full px-3 flex-0 mt-2 text-black">청구 확정하시겠습니까?</div>
@@ -112,7 +125,13 @@
 </CodeExampleFrame>
 
 <CodeExampleFrame name="Code2">
-	<iframe frameborder="0" scrolling="no" style="width:100%; height:397px;" allow="clipboard-write" src="https://emgithub.com/iframe.html?target=https%3A%2F%2Fgithub.com%2Fhippotnc-skku%2Ftemplate%2Fblob%2Fcomponent%2Fsrc%2Froutes%2F%28template%29%2Fmodal%2F_temp2.svelte%3Fts%3D4%23L1-L17&style=a11y-dark&type=code&showBorder=on&showLineNumbers=on&showFullPath=on&showCopy=on"></iframe>
+	<iframe
+		frameborder="0"
+		scrolling="no"
+		style="width:100%; height:397px;"
+		allow="clipboard-write"
+		src="https://emgithub.com/iframe.html?target=https%3A%2F%2Fgithub.com%2Fhippotnc-skku%2Ftemplate%2Fblob%2Fcomponent%2Fsrc%2Froutes%2F%28template%29%2Fmodal%2F_temp2.svelte%3Fts%3D4%23L1-L17&style=a11y-dark&type=code&showBorder=on&showLineNumbers=on&showFullPath=on&showCopy=on"
+	></iframe>
 </CodeExampleFrame>
 
 <CodeExampleFrame name="Options">
@@ -121,15 +140,15 @@
 			>open (필수)</Heading
 		>
 		<List tag="ul" class="w-full space-y-1 text-gray-500 dark:text-gray-400">
-			<Li>모달창이 나타나게 만드는 조건 </Li>
+			<Li>모달창이 나타나게 만드는 조건</Li>
 		</List>
 		<Heading tag="h2" customSize="text-lg font-semibold" class="text-lg font-semibold text-gray-900"
 			>내용</Heading
 		>
 		<List tag="ul" class="space-y-1 text-gray-500 dark:text-gray-400">
-			<Li> div 처리</Li>
-			<Li> form 처리</Li>
-			<Li> 버튼 처리</Li>
+			<Li>div 처리</Li>
+			<Li>form 처리</Li>
+			<Li>버튼 처리</Li>
 		</List>
 	</HippoWhiteFrame>
 </CodeExampleFrame>
